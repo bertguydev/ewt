5788adbeb821b66d359418a2ddacdaa7c4b4d72c
bert guy <bertcaivano@gmail.com>
2020-12-20 17:25:24 -0800

    updating build infrastructure. setup scss and css files. Added basic header

---
+++ src/components/header/header.php
@@ -0,0 +1,20 @@
+<header class="w100">
+    <div class="container">
+        <div class="d-flex w100 justify-content-between">
+            <div class="col-4">
+                Easy Way Transportation
+            </div>
+            <div class="col-6">
+                <nav class="d-flex justify-content-end">
+                    <div class="nav-item">Home</div>
+                    <div class="nav-item">About</div>
+                    <div class="nav-item">Services</div>
+                    <div class="nav-item">Industries Served</div>
+                    <div class="nav-item">Areas</div>
+                    <div class="nav-item">Contact Us</div>
+                    <div class="nav-item">Request Quote</div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
--- src/index.php
+++ src/index.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Easy Way Transportation</title>
+    <?php include 'components/header/header-css-paths.php' ?>
 </head>
 <body>
-    <?php include 'header.php'; ?>
+    <?php include 'components/header/header.php'; ?>
 </body>
 </html>
