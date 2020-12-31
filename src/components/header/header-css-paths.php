f638a65d9997380f5de4c8d43b0a62070695afdf
bert guy <bertcaivano@gmail.com>
2020-12-31 14:25:56 -0800

    setup html for request a quote form

--- src/components/header/header-css-paths.php
+++ src/components/header/header-css-paths.php
@@ -1,5 +1,5 @@
 <?php if(empty($is_home)){ ?>
-    <link rel="stylesheet" href="../assets/css/styles/style.css">
+    <link rel="stylesheet" href="../assets/css/styles/style.css?1.0.2">
     <link rel="stylesheet" href="../assets/css/styles/sub-pages.css">
 <?php }else { ?>
     <link rel="stylesheet" href="assets/css/styles/style.css">
--- src/request-quote/index.php
+++ src/request-quote/index.php
@@ -47,17 +47,54 @@ $seo_title = 'Request a Quote For Trucking From Easy Way Transportation';
         width: 100%;
         border-radius: 50%;
     }
+
+    .sub-page input {
+        margin: 0 0 20px;
+        padding: 5px 3px;
+        border-radius: 3px;
+        border-bottom: 1px solid #aaa;
+    }
+
 </style>
-<body class="about">
+<body class="request-quote-page sub-page">
     <?php include '../components/header/header.php'; ?>
     <?php include '../components/header/sub-page-title.php'; ?>
 
     <main>
         <div class="container">
-            <div class="panel">
-                <h4 class="text-center">Fill Out The Form Below and a representative will call or email your quote.</h4>
-                <form action="">
-                </form>
+            <div class="panel d-flex justify-content-between">
+                <div class="col-3">
+                    <h3>How It Works:</h3>
+                    <div class="d-flex flex-column">
+                        <div class="quote-steps">
+                            <h4>Step 1: Name</h4>
+                        </div>
+                        <div class="quote-steps">
+                            <h4>Step 2: Shipment Details</h4>
+                        </div>
+                        <div class="quote-steps">
+                            <h4>Step 3: Contact Information</h4>
+                        </div>
+                        <div class="quote-steps">
+                            <h4>Step 4: We'll Send Your Quote</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-9">
+                    <h4 class="text-center">Fill Out The Form Below and a representative will call or email your quote.</h4>
+                    <form action="" method="POST" class="d-flex justify-content-around">
+                        <input class="col-6" type="text" name="first_name" placeholder="First Name" required>
+                        <input class="col-6" type="text" name="last_name" placeholder="Last Name" required>
+                        <input class="col-6" type="text" name="company_name" placeholder="Company Name" required>
+                        <input class="col-6" type="text" name="pickup_address" placeholder="Pickup Address">
+                        <input class="col-6" type="text" name="destination_address" placeholder="Destination Address">
+                        <input class="col-6" type="text" name="weight_amount" placeholder="Weight (in lbs)">
+                        <input class="col-6" type="text" name="pallet_count" placeholder="Pallet Count">
+                        <input class="col-6" type="email" name="email_address" placeholder="Email" required>
+                        <input class="col-12" type="text" name="phone" placeholder="Phone Number" required>
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
             </div>    
         </div>
     </main>
